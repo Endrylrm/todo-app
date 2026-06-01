31491ef4edd86c6fce91503b8a4e65795face24b
Endryl Richard Monteiro <endrylrm@hotmail.com>
2026-06-01 18:41:11 -0300

    Add Todo API stubs.

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,34 @@
-def main():
-    print("Hello from backend!")
+from fastapi import FastAPI
+
+app = FastAPI()
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+@app.get("/api")
+async def root():
+    return {"message": "Welcome to todo api!"}
+
+
+@app.get("/api/todos")
+async def get_todos():
+    return {}
+
+
+@app.get("/api/todos/{id}")
+async def get_todo(id: int):
+    return {}
+
+
+@app.patch("/api/todos/{id}")
+async def update_todo(id: int, todo: dict):
+    return {}
+
+
+@app.put("/api/todos/{id}")
+async def update_todo_completely(id: int, todo: dict):
+    return {}
+
+
+@app.delete("/api/todos/{id}")
+async def delete_todo(id: int):
+    return {}
