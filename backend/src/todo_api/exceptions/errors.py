eba75c7f8b8a8ce58a57b5613b9f21ff2acd343a
Endryl Richard Monteiro <endrylrm@hotmail.com>
2026-06-03 23:58:49 -0300

    Backend: oops, small mistake put self, instead of super

--- backend/src/todo_api/exceptions/errors.py
+++ backend/src/todo_api/exceptions/errors.py
@@ -1,13 +1,13 @@
 class TodoNotFoundError(Exception):
     def __init__(self, id: int):
         self.id = id
-        self().__init__(f"Todo with id: {self.id} not found!")
+        super().__init__(f"Todo with id: {self.id} not found!")
 
 
 class TodoInvalidDataError(Exception):
     def __init__(self, variable: str):
         self.variable = variable
-        self().__init__(f"No '{self.variable}' in request!")
+        super().__init__(f"No '{self.variable}' in request!")
 
 
 class TodoEmptyDataError(Exception):
