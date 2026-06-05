c10b4aa6532535700aca02a7bbbe6afeec46713b
Endryl Richard Monteiro <endrylrm@hotmail.com>
2026-06-04 23:07:14 -0300

    Frontend: move todo list to class init.

--- frontend/src/todo_gui/pages/todo_page.py
+++ frontend/src/todo_gui/pages/todo_page.py
@@ -8,18 +8,18 @@ from services.api_client_service import APIClientService
 class TodoPage:
     def __init__(self, api_client: APIClientService):
         self._api_client = api_client
+        self.todos = self._api_client.get_todos()
 
         self.render()
 
     def render(self):
-        todos = self._api_client.get_todos()
         with ui.row():
             with ui.link(target="/add"):
                 ui.button("Add Task", icon="add", color="green")
         with ui.grid(columns=4).classes("w-full gap-4"):
-            if not todos:
+            if not self.todos:
                 ui.label(
                     "No task yet, please click on 'Add Task' to add a new one"
                 ).classes("text-4xl col-span-4")
-            for todo in todos:
+            for todo in self.todos:
                 TodoCard(todo, self._api_client)
