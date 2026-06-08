f8efe3a9a393af4ca43b3a0c88c0f7e71b60510a
Endryl Richard Monteiro <endrylrm@hotmail.com>
2026-06-08 13:46:26 -0300

    Backend: use context manager for safer queries.

--- backend/src/todo_api/repositories/todo_sqlite_repository.py
+++ backend/src/todo_api/repositories/todo_sqlite_repository.py
@@ -17,16 +17,16 @@ from ..exceptions.errors import (
 
 class TodoSQLiteRepository(TodoRepository):
     def __init__(self, connection: sqlite3.Connection):
-        self._conn = connection
+        self._connection = connection
 
     async def get_all(self) -> Any:
-        cursor = self._conn.cursor()
+        cursor = self._connection.cursor()
         cursor.execute("SELECT * FROM todos")
         todos = cursor.fetchall()
         return todos
 
     async def get_one(self, id: str) -> Any:
-        cursor = self._conn.cursor()
+        cursor = self._connection.cursor()
         cursor.execute("SELECT * FROM todos WHERE id = ?", (id,))
         todo = cursor.fetchone()
         return todo
@@ -35,16 +35,16 @@ class TodoSQLiteRepository(TodoRepository):
         self._check_empty_todo(todo)
         self._validate_todo(todo)
 
-        cursor = self._conn.cursor()
-        cursor.execute(
-            "INSERT INTO todos (title, description, is_active) VALUES (?, ?, ?)",
-            (
-                str(todo.title),
-                str(todo.description),
-                int(todo.is_active),
-            ),
-        )
-        self._conn.commit()
+        with self._connection:
+            cursor = self._connection.cursor()
+            cursor.execute(
+                "INSERT INTO todos (title, description, is_active) VALUES (?, ?, ?)",
+                (
+                    str(todo.title),
+                    str(todo.description),
+                    int(todo.is_active),
+                ),
+            )
 
     async def insert_many(self, todos: list[Todo]):
         for todo in todos:
@@ -55,12 +55,11 @@ class TodoSQLiteRepository(TodoRepository):
         self._check_empty_todo(todo)
 
         # check if todo exist
-        todo = self.get_one(id)
+        todo = await self.get_one(id)
 
         if todo is None:
             raise TodoNotFoundError(id)
 
-        cursor = self._conn.cursor()
         sql = "UPDATE todos SET "
         temp_parameter_list = []
 
@@ -82,11 +81,12 @@ class TodoSQLiteRepository(TodoRepository):
         sql = sql + " WHERE id = ?"
         temp_parameter_list.append(id)
 
-        cursor.execute(
-            sql,
-            tuple(temp_parameter_list),
-        )
-        self._conn.commit()
+        with self._connection:
+            cursor = self._connection.cursor()
+            cursor.execute(
+                sql,
+                tuple(temp_parameter_list),
+            )
 
     async def update_many(self, todos: list[Todo]):
         for todo in todos:
@@ -96,38 +96,38 @@ class TodoSQLiteRepository(TodoRepository):
         self._check_empty_todo(todo)
         self._validate_todo(todo)
 
-        cursor = self._conn.cursor()
-        cursor.execute(
-            """
-            INSERT INTO todos (id, title, description, is_active, updated_at) 
-            VALUES (?, ?, ?, ?, ?)
-            ON CONFLICT(id)
-            DO UPDATE SET
-                title = excluded.title,
-                description = excluded.description,
-                is_active = excluded.is_active,
-                updated_at = excluded.updated_at;
-            """,
-            (
-                id,
-                str(todo.title),
-                str(todo.description),
-                int(todo.is_active),
-                datetime.now(UTC).isoformat(),
-            ),
-        )
-        self._conn.commit()
+        with self._connection:
+            cursor = self._connection.cursor()
+            cursor.execute(
+                """
+                INSERT INTO todos (id, title, description, is_active, updated_at) 
+                VALUES (?, ?, ?, ?, ?)
+                ON CONFLICT(id)
+                DO UPDATE SET
+                    title = excluded.title,
+                    description = excluded.description,
+                    is_active = excluded.is_active,
+                    updated_at = excluded.updated_at;
+                """,
+                (
+                    id,
+                    str(todo.title),
+                    str(todo.description),
+                    int(todo.is_active),
+                    datetime.now(UTC).isoformat(),
+                ),
+            )
 
     async def delete_one(self, id: str):
         # check if todo exist
-        todo = self.get_one(id)
+        todo = await self.get_one(id)
 
         if todo is None:
             raise TodoNotFoundError(id)
 
-        cursor = self._conn.cursor()
-        cursor.execute("DELETE FROM todos WHERE id = ?", (id,))
-        self._conn.commit()
+        with self._connection:
+            cursor = self._connection.cursor()
+            cursor.execute("DELETE FROM todos WHERE id = ?", (id,))
 
     async def delete_many(self, ids: list[str]):
         for id in ids:
